8cdc247d451c0db8078ee109c5a13851d3b86bdb
mohamadkai <mohamadaly990@gmail.com>
2026-05-17 15:15:27 +0300

    Configure Vercel deployment with Nitro

---
+++ .vercel/output/functions/__server.func/_tanstack-start-manifest_v-DyUtSk_G.mjs
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/moham/Downloads/New folder (16)/umami-moments-main/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-C-OSazhW.js"] }, "/": { filePath: "C:/Users/moham/Downloads/New folder (16)/umami-moments-main/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-rMwOUYp0.js"] } }, clientEntry: "/assets/index-C-OSazhW.js" });
+export {
+  tsrStartManifest
+};
